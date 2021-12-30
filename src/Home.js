cff848a522080edc118fc4e63191c6122bccc60f
almeizaarvin <13519066@std.stei.itb.ac.id>
2021-12-30 18:55:12 +0700

    Add JSON server

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,14 +8,22 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [])
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
     </div>
   );
 }
