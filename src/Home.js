c0c0604dd47d847293890b693ee3ae56f4ec0c49
almeizaarvin <13519066@std.stei.itb.ac.id>
2022-01-04 13:44:57 +0700

    Add fetch component

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react";
+
+  //npx json-server --watch data/db.json --port 8000
+
+  import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-    }, 1000);
-  }, [])
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} />}
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
