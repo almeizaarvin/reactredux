cd735280a032f8f1d80dea3dc155d4eefbd2c94c
almeizaarvin <13519066@std.stei.itb.ac.id>
2022-01-04 14:40:20 +0700

    Add BrowserRoute

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'http://www.google.com';
-
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
